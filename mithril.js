caf6ea77acea012213ee602fb4e2cfc97ee3b47f
Barney Carroll <barney.carroll@gmail.com>
2016-07-29 15:30:30 +0100

    Nested onbeforeremove delays ancestor's onremove

--- mithril.js
+++ mithril.js
@@ -355,7 +355,6 @@ var renderService = function($window) {
 		else {
 			var recycling = isRecyclable(old, vnodes)
 			if (recycling) old = old.concat(old.pool)
-			
 			if (old.length === vnodes.length && vnodes[0] != null && vnodes[0].key == null) {
 				for (var i = 0; i < old.length; i++) {
 					if (old[i] == null && vnodes[i] == null) continue
@@ -573,13 +572,22 @@ var renderService = function($window) {
 			var callback = function() {
 				if (++called === expected) removeNode(parent, vnode, context, true)
 			}
-			if (vnode.attrs && vnode.attrs.onbeforeremove) {
-				expected++
-				vnode.attrs.onbeforeremove.call(vnode.state, vnode, callback)
-			}
-			if (typeof vnode.tag !== "string" && vnode.tag.onbeforeremove) {
-				expected++
-				vnode.tag.onbeforeremove.call(vnode.state, vnode, callback)
+			var removables = [vnode]
+			while (removables[0]) {
+				var removable = removables.shift()
+				if (removable.attrs && removable.attrs.onbeforeremove) {
+					expected++
+					removable.attrs.onbeforeremove.call(removable.state, removable, callback)
+				}
+				if (removable.tag && typeof removable.tag !== "string" && removable.tag.onbeforeremove) {
+					expected++
+					removable.tag.onbeforeremove.call(removable.state, removable, callback)
+				}
+				if (removable.children && removable.children.length) {
+					for (var i = 0; i < removable.children.length; i++) {
+						removables.push(removable.children[i])
+					}
+				}
 			}
 			if (expected > 0) return
 		}
@@ -593,7 +601,7 @@ var renderService = function($window) {
 				}
 			}
 			if (vnode.dom.parentNode != null) parent.removeChild(vnode.dom)
-			if (context != null && vnode.domSize == null && !hasIntegrationMethods(vnode.attrs) && typeof vnode.tag === "string") { //TODO test custom elements
+			if (context != null && vnode.domSize == null && !hasIntegrationMethods(vnode.attrs) && typeof vnode.tag === "string" && vnode.tag !== "<") { //TODO test custom elements
 				if (!context.pool) context.pool = [vnode]
 				else context.pool.push(vnode)
 			}
